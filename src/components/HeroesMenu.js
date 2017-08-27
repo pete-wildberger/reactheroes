c6204722a3718383bb2aa6ee41d934e45416c35a
Peter Wildberger <pete.wildberger@gmail.com>
2017-08-27 17:45:27 -0500

    adding bootstrap, limited success

--- src/components/HeroesMenu.js
+++ src/components/HeroesMenu.js
@@ -6,13 +6,15 @@ import { Link } from 'react-router';
 export default class HeroesMenu extends React.Component {
   render() {
     return (
-      <nav>
-        {this.props.heroes.map(menuHero => {
-          return <Link key={menuHero.id} to={`/hero/${menuHero.id}`} activeClassName="active">
-            {menuHero.name}
-          </Link>;
-        })}
-      </nav>
+        <div className="row">
+          <div className="col-md-4">
+            {this.props.heroes.map(menuHero => {
+                return <Link key={menuHero.id} to={`/hero/${menuHero.id}`} activeClassName="active">
+                {menuHero.name}
+                </Link>;
+              })}
+              </div>
+        </div>
     );
   }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,13 +6,16 @@ import { Link } from 'react-router';
 export default class Layout extends React.Component {
   render() {
     return (
-      <div className="app-container">
+      <div className="container">
         <header>
           <Link to="/">
             <img className="logo" src="/img/logo-judo-heroes.png"/>
           </Link>
         </header>
-        <div >{this.props.children}</div>
+        <div className="row">
+        <div className="col-md-4">{this.props.children}</div>
+        <footer>Hello</footer>
+        </div>
       </div>
     );
   }
